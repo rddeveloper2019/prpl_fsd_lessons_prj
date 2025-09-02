519d9eb4ff1277232dbc212b6a8f37a6c3fc874f
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-09-02 23:37:06 +0300

    init fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Привет из FSD</div>,
+  },
+]);
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);
